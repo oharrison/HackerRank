0e7f502612758571e3d5726bf12153da55d3b746
onel <ogh.developer@gmail.com>
2016-08-27 15:43:01 -0400

    Add C++/Introduction/input_and_output.cpp

---
+++ C++/Introduction/input_and_output.cpp
@@ -0,0 +1,15 @@
+/**
+ * Author: Onel Harrison
+ * HackerRank: C++ > Introduction > Input and Output
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  int a = 0, b = 0, c = 0;
+  cin >> a >> b >> c;
+  cout << a + b + c << endl;
+  return 0;
+}
