9d49ab67439a96c94a6fbe82d37b60572fbd8a12
onel <ogh.developer@gmail.com>
2016-08-27 15:33:27 -0400

    Add C++/Introduction hello_world.cpp

---
+++ C++/Introduction/hello_world.cpp
@@ -0,0 +1,13 @@
+/**
+ * Author: Onel Harrison
+ * HackerRank: C++ > Introduction > Say "Hello, World!" With C++
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, World!";
+  return 0;
+}
